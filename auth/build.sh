7071b2ecba23adcc91b8d3d03a047aded3dd8fe3
Arnavonawhim <agrawal.arnav20@gmail.com>
2025-11-11 20:27:23 +0530

    fix #4

--- auth/build.sh
+++ auth/build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Installing system dependencies..."
-apt-get update
-apt-get install -y libjpeg-dev zlib1g-dev libpng-dev libtiff-dev libfreetype6-dev liblcms2-dev libwebp-dev libopenjp2-7-dev
-
 echo "Installing Python dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
@@ -15,4 +11,4 @@ python manage.py collectstatic --no-input
 echo "Running migrations..."
 python manage.py migrate
 
-echo "Build completed successfully! "
+echo "Build completed successfully!"
