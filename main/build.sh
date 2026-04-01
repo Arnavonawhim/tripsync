d14d924cb511b99d940a0bafcd4ef7122d870e27
Arnav Agrawal <agrawal.arnav20@gmail.com>
2026-04-01 17:47:03 +0530

    updated build.sh

---
+++ main/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -o errexit
+
+echo "Installing dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+pip install gunicorn
+
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+echo "Running migrations..."
+python manage.py migrate
+
+echo "Build completed successfully!"
