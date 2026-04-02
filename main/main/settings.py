1514608a0750ceb3172c9258922d481a80142fe2
Arnav Agrawal <agrawal.arnav20@gmail.com>
2026-04-03 05:04:05 +0530

    added Static root file

--- main/main/settings.py
+++ main/main/settings.py
@@ -6,6 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS',cast=lambda v: [s.strip() for s in v.split(',')])
